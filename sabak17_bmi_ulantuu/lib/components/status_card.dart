bc3b63a6f0637beca48598927782b725b9216d21
Sadyrbek <sadyrbek.momunaliev.bj@gmail.com>
2023-04-30 00:37:39 +0200

    UI buttu

---
+++ sabak17_bmi_ulantuu/lib/components/status_card.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+import '../theme/app_colors.dart';
+
+class StatusCard extends StatelessWidget {
+  const StatusCard({super.key, required this.icon, required this.text});
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        color: AppColors.cardColor,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 80,),
+            Text(text, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ sabak17_bmi_ulantuu/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:sabak17_bmi_ulantuu/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
